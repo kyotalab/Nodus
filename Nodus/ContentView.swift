bf970a53e1f550ac6aa485b7464c8d2b074393e7
kyotalab <jagbvg.eptpeox@gmail.com>
2026-05-03 20:57:48 +0900

    Initial Commit

---
+++ Nodus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Nodus
+//
+//  Created by 中田恭大 on 2026/05/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Nodus/NodusApp.swift
@@ -0,0 +1,17 @@
+//
+//  NodusApp.swift
+//  Nodus
+//
+//  Created by 中田恭大 on 2026/05/03.
+//
+
+import SwiftUI
+
+@main
+struct NodusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
